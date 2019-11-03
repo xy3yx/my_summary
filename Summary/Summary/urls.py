653028a1d12776c7c0041fd2a2753681509b747b
Ivan Kravets <kravetsis@yandex.ru>
2019-11-03 17:08:24 +0300

    Created first_experience page

--- Summary/Summary/urls.py
+++ Summary/Summary/urls.py
@@ -19,6 +19,5 @@ from django.conf.urls import url, include
 
 urlpatterns = {
     url(r'admin/', admin.site.urls),
-    url(r'^$', include('MySummary.urls')),
-    url(r'first_exp/', include('MySummary.urls'))
+    url(r'^', include('MySummary.urls')),
 }
