d5d3aa425b7b416e1d834454d04901c181668277
Ivan Kravets <kravetsis@yandex.ru>
2019-11-03 17:23:06 +0300

    Created third experience page

--- Summary/MySummary/urls.py
+++ Summary/MySummary/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
         url(r'^$', views.index, name='index'),
         url(r'^first_exp/', views.experience, name='first_exp'),
-        url(r'^second_exp/', views.second_experience, name='second_exp')
+        url(r'^second_exp/', views.second_experience, name='second_exp'),
+        url(r'^third_exp/', views.third_experience, name='third_exp')
 ]
--- Summary/MySummary/views.py
+++ Summary/MySummary/views.py
@@ -9,5 +9,8 @@ def experience(request):
 def second_experience(request):
     return render(request, 'second_exp.html')
 
+def third_experience(request):
+    return render(request, 'third_exp.html')
+
 # Create your views here.
 
